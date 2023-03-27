d2c22881bafbace2ea6851d4834296b2fb3113b0
Bryan <bryantoromo@gmail.com>
2023-03-27 13:43:53 +0300

    create mock price fetch service

---
+++ service.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type PriceFetcher interface {
+	FetchPrice(context.Context, string) (float64, error)
+}
+
+type priceFetcher struct{}
+
+func (s *priceFetcher) FetchPrice(ctx context.Context, ticker string) (float64, error) {
+	return MockPriceFetching(ctx, ticker)
+
+}
+
+var priceMock = map[string]float64{
+	"BTC": 20000.0,
+	"ETH": 200.1,
+	"RPX": 100.5,
+}
+
+func MockPriceFetching(ctx context.Context, ticker string) (float64, error) {
+	price, ok := priceMock[ticker]
+
+	if !ok {
+		return price, fmt.Errorf("the given ticker (%s) does not exist", ticker)
+	}
+
+	return price, nil
+}
